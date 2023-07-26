396f1372ef83737e418f7e89baac62c533335cc8
naykony <140612794+naykony@users.noreply.github.com>
2023-07-26 15:31:04 +0330

    Update webb.py

--- webb.py
+++ webb.py
@@ -2,9 +2,8 @@ import threading
 import av
 import cv2
 import streamlit as st
-from matplotlib import pyplot as plt
-from sample_utils.turn import get_ice_servers
-from sample_utils.download import download_file
+#from sample_utils.turn import get_ice_servers
+#from sample_utils.download import download_file
 from streamlit_webrtc import (
     RTCConfiguration,
     WebRtcMode,
@@ -30,7 +29,7 @@ def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
 webrtc_ctx=webrtc_streamer(
             key="object-detection",
             mode=WebRtcMode.SENDRECV,
-            rtc_configuration={"iceServers": get_ice_servers()},
+        #    rtc_configuration={"iceServers": get_ice_servers()},
             video_frame_callback=video_frame_callback,
             media_stream_constraints={"video": True, "audio": False},
             async_processing=True
